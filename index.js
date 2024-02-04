6840d461dd6562cfc43a8d79d47803a32dd0b9aa
FoxMoss <foxmoss@mediaology.com>
2024-02-04 00:36:46 -0600

    Cleanup Exports

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
 import bindings from 'bindings';
-import express from 'express';
 import { WebSocketServer } from 'ws';
-const wisp = bindings('wispservercpp')
-
-const wsServer = new WebSocketServer({ noServer: true });
+export const wisp = bindings('wispservercpp');
 
 const socketMaps = {};
 
 const sendCallback = (exit, msg, id) => {
-  let socket = socketMaps[id]
-  socket.send(msg)
+  let socket = socketMaps[id];
+  socket.send(msg);
   if (exit) {
-    socket.close()
+    socket.close();
   }
 }
 
-
-wsServer.on('connection', socket => {
-  let id = wisp.NextID();
-  socketMaps[id] = socket;
-  wisp.Open(id, sendCallback)
-
-  socket.on('message', message => wisp.Message(id, message.toString(), sendCallback));
-});
+const wsServer = new WebSocketServer({ noServer: true });
 
 export const routeUpgrade = (req, socket, head) => {
   wsServer.handleUpgrade(req, socket, head, socket => {
     wsServer.emit('connection', socket, req);
   });
 }
+export const init = () => {
+  wisp.Init(sendCallback);
+  wsServer.on('connection', socket => {
+    let id = wisp.NextID();
+    socketMaps[id] = socket;
+    wisp.Open(id, sendCallback)
 
+    socket.on('message', message => wisp.Message(id, message.toString(), sendCallback));
+  });
 
-wisp.Init(sendCallback);
-
-console.log("Runnning Wisp Server on 6001");
-const app = express();
-const server = app.listen(6001);
-server.on('upgrade', (request, socket, head) => {
-  routeUpgrade(request, socket, head)
-});
+}
