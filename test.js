cf897cc2867daab6832285f7d5e26157bc0d5c7b
FoxMoss <foxmoss@mediaology.com>
2024-02-26 16:09:10 -0600

    Bump!

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { init, routeUpgrade } from "./index"
+import { init, routeUpgrade } from "./index.js"
 import http from 'node:http'
 
 init();
@@ -6,6 +6,6 @@ const server = http.createServer();
 server.on('upgrade', (request, socket, head) => {
   routeUpgrade(request, socket, head);
 });
-server.listen(6001)
+server.listen(4000)
 console.log("Runnning Wisp Server on 6001");
 
